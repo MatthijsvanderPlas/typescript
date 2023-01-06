be727f9eb676086086dc3f880f7f3f370e5a065e
matthijsvanderplas <mvdplas84@ziggo.nl>
2023-01-06 09:58:44 +0100

    Webpack setup

--- src/components/project-item.ts
+++ src/components/project-item.ts
@@ -1,7 +1,7 @@
-import { Component } from './base-component.js'
-import { Project } from '../models/project.js';
-import { Draggable } from '../models/dragdrop.js'
-import { Autobind } from '../decorators/autobind.js';
+import { Component } from './base-component'
+import { Project } from '../models/project';
+import { Draggable } from '../models/dragdrop'
+import { Autobind } from '../decorators/autobind';
 
 
 // ProjectItem Class
